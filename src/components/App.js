6a86be549bed62f5b0f17c8ffb763952082bcec7
Leo <leonid.samograew@gmail.com>
2021-12-12 20:15:48 +0300

    created first structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Screen1 from './Screen1/Screen1';
+import Benefits from './Screen2/Benefits';
+import WorkExamples from './Screen3/WorkExamples';
+import CalculateAndOrder from './Screen4/CalculateAndOrder';
 
 
 export default function App() {
   return (
     <div className="App">
-      APP
+      <Screen1 />
+      <Benefits />
+      <WorkExamples />
+      <CalculateAndOrder />
     </div>
   );
 }
