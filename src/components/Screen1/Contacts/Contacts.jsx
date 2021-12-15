909320efd05c6f1993d77ed59e969e3c0b948587
Leo <leonid.samograew@gmail.com>
2021-12-15 14:33:14 +0300

    Contacts --->>> created Logo,Phone components

--- src/components/Screen1/Contacts/Contacts.jsx
+++ src/components/Screen1/Contacts/Contacts.jsx
@@ -1,9 +1,13 @@
 import './Contacts.css';
+import Logo from './Logo/Logo';
+import Phone from './Phone/Phone';
+
 
 export default function Contacts() {
   return (
     <div className="Contacts">
-      123
+      <Logo />
+      <Phone />
     </div>
   );
 }
---
+++ src/components/Screen1/Contacts/Phone/Phone.jsx
@@ -0,0 +1,10 @@
+import './Phone.css';
+
+export default function Phone() {
+  return (
+    <div className="Phone">
+      <div>+7 (9) 81 82 83 801</div>
+    </div>
+  );
+}
+
