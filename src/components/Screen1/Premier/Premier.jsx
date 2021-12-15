6e1d4e6291d747342177fcfb35fdff6a8fb50769
Leo <leonid.samograew@gmail.com>
2021-12-15 16:06:36 +0300

    Screen1 --->>> created Premier component

---
+++ src/components/Screen1/Premier/Premier.jsx
@@ -0,0 +1,14 @@
+import './Premier.css';
+
+export default function Premier() {
+  return (
+    <div className="Premier">
+      <div className='Premier-content'>
+        <div className='lineOne'>САЙТЫ ПОД КЛЮЧ</div>
+        <div className='lineTwo'>ОТ <b>4 990</b> РУБЛЕЙ</div>
+        <button>ЗАКАЗАТЬ ЗВОНОК</button>
+      </div>
+    </div>
+  );
+}
+
--- src/components/Screen1/Screen1.jsx
+++ src/components/Screen1/Screen1.jsx
@@ -1,10 +1,13 @@
 import './Screen1.css';
 import Contacts from './Contacts/Contacts';
+import Premier from './Premier/Premier';
+
 
 export default function Screen1() {
   return (
     <div className="Screen1">
       <Contacts />
+      <Premier />
     </div>
   );
 }
