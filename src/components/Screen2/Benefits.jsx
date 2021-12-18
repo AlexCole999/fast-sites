b803f408fe98a7ff0aea96b6122a0c81ca8e68db
Leo <leonid.samograew@gmail.com>
2021-12-18 14:26:59 +0300

    Benefits --->>> flex-direction:column, align-items:center

--- src/components/Screen2/Benefits.jsx
+++ src/components/Screen2/Benefits.jsx
@@ -5,6 +5,12 @@ export default function Benefits() {
   return (
     <div className="benefits-wrapper">
       <BenefitsTitle />
+      <div className="benefitsMenu">
+        <div className="benefitsElem">1</div>
+        <div className="benefitsElem">2</div>
+        <div className="benefitsElem">3</div>
+        <div className="benefitsElem">4</div>
+      </div>
     </div>
   );
 }
