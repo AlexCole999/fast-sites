2851a6a0606e2c5f3d78b73bbb043db1cbed3788
Leo <leonid.samograew@gmail.com>
2021-12-19 19:25:22 +0300

    Screen3 --->>> complete examples structure

--- src/components/Screen3/WorkExamples.jsx
+++ src/components/Screen3/WorkExamples.jsx
@@ -1,5 +1,13 @@
 import './WorkExamples.css';
 import WorkExamplesTitle from './WorkExamplesTitle/WorkExamplesTitle';
+import example1 from '../../images/ourworks1.jpg'
+import example2 from '../../images/statistics.jpg'
+import example3 from '../../images/background-v2.jpg'
+import example4 from '../../images/order.jpg'
+import example5 from '../../images/works.jpg'
+
+
+
 
 export default function WorkExamples() {
   return (
@@ -7,13 +15,33 @@ export default function WorkExamples() {
       <WorkExamplesTitle />
       <div className="examples">
         <div className="row1">
-          <div className="elem"></div>
-          <div className="elem"></div>
+          <div className="row-elem">
+            <div>
+              <img src={example2} alt="" />
+            </div>
+          </div>
+          <div className="row-elem">
+            <div>
+              <img src={example3} alt="" />
+            </div>
+          </div>
         </div>
         <div className="row2">
-          <div className="elem"></div>
-          <div className="elem"></div>
-          <div className="elem"></div>
+          <div className="row-elem">
+            <div>
+              <img src={example4} alt="" />
+            </div>
+          </div>
+          <div className="row-elem">
+            <div>
+              <img src={example5} alt="" />
+            </div>
+          </div>
+          <div className="row-elem">
+            <div>
+              <img src={example4} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
