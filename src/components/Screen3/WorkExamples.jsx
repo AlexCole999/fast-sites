a69bd7d0f133a3516781de4eec6c32356554786e
Leo <leonid.samograew@gmail.com>
2021-12-19 18:53:44 +0300

    Screen3 --->>> created background properties within elements of examples

--- src/components/Screen3/WorkExamples.jsx
+++ src/components/Screen3/WorkExamples.jsx
@@ -5,6 +5,17 @@ export default function WorkExamples() {
   return (
     <div className="WorkExamples">
       <WorkExamplesTitle />
+      <div className="examples">
+        <div className="row1">
+          <div className="elem"></div>
+          <div className="elem"></div>
+        </div>
+        <div className="row2">
+          <div className="elem"></div>
+          <div className="elem"></div>
+          <div className="elem"></div>
+        </div>
+      </div>
     </div>
   );
 }
