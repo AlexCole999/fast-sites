30655f628e71094b1dc32d43e251455607ecdd18
Leo <leonid.samograew@gmail.com>
2021-12-20 21:10:06 +0300

    created OrderForm & OrderSum components

--- src/components/Screen4/Order/OrderForm/OrderSum/OrderSum.jsx
+++ src/components/Screen4/Order/OrderForm/OrderSum/OrderSum.jsx
@@ -2,11 +2,10 @@ import './OrderSum.css';
 
 export default function OrderSum() {
   return (
-    <div className="OrderSum"
-        style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
-        <div> Итого</div>
-        <div> 99 999</div>
-      </div>
+    <div className="OrderSum">
+      <div className='OrderSum-resultWord'> Итого</div>
+      <div className='OrderSum-resultPrice'> 18 990</div>
+    </div>
   );
 }
 
