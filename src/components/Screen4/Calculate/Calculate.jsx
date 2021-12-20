7f4cdc792a6b01ee141d385011ae50b5a1ed4854
Leo <leonid.samograew@gmail.com>
2021-12-20 20:50:40 +0300

    Service & Calculate --->>> fixed some styles

--- src/components/Screen4/Calculate/Calculate.jsx
+++ src/components/Screen4/Calculate/Calculate.jsx
@@ -4,29 +4,13 @@ import Service from './Service/Service';
 export default function Calculate() {
   return (
     <div className="Calculate">
-      <Service text={"3 главных экрана"} price={"4 990"} />
-      <Service text={"Мобильный адаптив"} price={"2 290"} />
-      <Service text={"Изменяемый контент"} price={"3 390"} />
+      <Service text={"3 главных страницы + адаптив"} price={"6 990"} />
+      <Service text={"3 главных страницы"} price={"4 990"} />
+      <Service text={"Дополнительная страница + адаптив"} price={"1 590"} />
       <Service text={"Дополнительная страница"} price={"990"} />
-      <Service text={<div><div>Дополнительная страница</div><div>(+адаптив под устройства)</div></div>} price={"1 590"} />
-      <Service text={"Продвижение"} price={"7 990"} />
-      <Service text={"3 главных экрана"} price={"4 990"} />
-      <Service text={"Мобильный адаптив"} price={"2 290"} />
-      <Service text={"Изменяемый контент"} price={"3 390"} />
-      <Service text={"Дополнительная страница"} price={"990"} />
-      <Service text={<div><div>Дополнительная страница</div><div>(+адаптив под устройства)</div></div>} price={"1 590"} />
-      <Service text={"Продвижение"} price={"7 990"} />
-      <Service text={"3 главных экрана"} price={"4 990"} />
-      <Service text={"Мобильный адаптив"} price={"2 290"} />
-      <Service text={"Изменяемый контент"} price={"3 390"} />
-      <Service text={"Дополнительная страница"} price={"990"} />
-      <Service text={<div><div>Дополнительная страница</div><div>(+адаптив под устройства)</div></div>} price={"1 590"} />
-      <Service text={"Продвижение"} price={"7 990"} />
-      <Service text={"3 главных экрана"} price={"4 990"} />
-      <Service text={"Мобильный адаптив"} price={"2 290"} />
-      <Service text={"Изменяемый контент"} price={"3 390"} />
-      <Service text={"Дополнительная страница"} price={"990"} />
-      <Service text={<div><div>Дополнительная страница</div><div>(+адаптив под устройства)</div></div>} price={"1 590"} />
+      <Service text={"Изменяемый контент (админ панель)"} price={"2 490"} />
+      <Service text={"Карта (google или Yandex)"} price={"790"} />
+      <Service text={"Оплата на сайте"} price={"3 990"} />
       <Service text={"Продвижение"} price={"7 990"} />
     </div>
   );
