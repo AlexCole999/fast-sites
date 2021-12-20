e7265454c43fdfcf7fafb13a5d3d860311b2f8f7
Leo <leonid.samograew@gmail.com>
2021-12-20 17:04:25 +0300

    Screen4 --->>> created Order & Calculate components

--- src/components/Screen4/Calculate/Calculate.jsx
+++ src/components/Screen4/Calculate/Calculate.jsx
@@ -3,7 +3,13 @@ import './Calculate.css';
 export default function Calculate() {
   return (
     <div className="Calculate">
-
+      <div className='Service'>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div className="service-checkbox"></div>
+          <div className="service-text">Lorem ipsum dolor sit amet consectetur a Lorem ipsum dolor sit amet consectetur a </div>
+        </div>
+        <div className="service-price">4 990</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Screen4/Calculate/Service/Service.jsx
@@ -0,0 +1,16 @@
+import './Service.css';
+
+export default function Service() {
+  return (
+    <div>
+      <div className='service'>
+        <div className="service-checkbox"></div>
+        <div className="service-text">УСЛУГА</div>
+      </div>
+      <div>
+        <div className="service-price">4 990</div>
+      </div>
+    </div>
+  );
+}
+
