82fd89143f4d28b5670b1e8ba66540a9a15e569a
Leo <leonid.samograew@gmail.com>
2021-12-20 18:02:42 +0300

    Calculate --->>> finish

--- src/components/Screen4/Calculate/Calculate.jsx
+++ src/components/Screen4/Calculate/Calculate.jsx
@@ -10,6 +10,24 @@ export default function Calculate() {
       <Service text={"Дополнительная страница"} price={"990"} />
       <Service text={<div><div>Дополнительная страница</div><div>(+адаптив под устройства)</div></div>} price={"1 590"} />
       <Service text={"Продвижение"} price={"7 990"} />
+      <Service text={"3 главных экрана"} price={"4 990"} />
+      <Service text={"Мобильный адаптив"} price={"2 290"} />
+      <Service text={"Изменяемый контент"} price={"3 390"} />
+      <Service text={"Дополнительная страница"} price={"990"} />
+      <Service text={<div><div>Дополнительная страница</div><div>(+адаптив под устройства)</div></div>} price={"1 590"} />
+      <Service text={"Продвижение"} price={"7 990"} />
+      <Service text={"3 главных экрана"} price={"4 990"} />
+      <Service text={"Мобильный адаптив"} price={"2 290"} />
+      <Service text={"Изменяемый контент"} price={"3 390"} />
+      <Service text={"Дополнительная страница"} price={"990"} />
+      <Service text={<div><div>Дополнительная страница</div><div>(+адаптив под устройства)</div></div>} price={"1 590"} />
+      <Service text={"Продвижение"} price={"7 990"} />
+      <Service text={"3 главных экрана"} price={"4 990"} />
+      <Service text={"Мобильный адаптив"} price={"2 290"} />
+      <Service text={"Изменяемый контент"} price={"3 390"} />
+      <Service text={"Дополнительная страница"} price={"990"} />
+      <Service text={<div><div>Дополнительная страница</div><div>(+адаптив под устройства)</div></div>} price={"1 590"} />
+      <Service text={"Продвижение"} price={"7 990"} />
     </div>
   );
 }
--- src/components/Screen4/Order/Order.jsx
+++ src/components/Screen4/Order/Order.jsx
@@ -3,7 +3,21 @@ import './Order.css';
 export default function Order() {
   return (
     <div className="Order">
-
+      <div className="Order-form">
+        <div className="input-elem">
+          <div className="order-phone">Телефон</div>
+          <input className="order-phone-input" type="text" />
+        </div>
+        <div className="input-elem">
+          <div className="order-name">Имя</div>
+          <input className="order-name-input" type="text" />
+        </div>
+      </div>
+      <div className="sum"
+        style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
+        <div> Итого</div>
+        <div> 99 999</div>
+      </div>
     </div>
   );
 }
