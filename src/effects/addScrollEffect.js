2ceddb4fb92cd67b8e619a5a57f25be46fc1b97d
Leo <leonid.samograew@gmail.com>
2021-12-25 12:46:22 +0300

    fixed addScrollEffect

--- src/effects/addScrollEffect.js
+++ src/effects/addScrollEffect.js
@@ -5,15 +5,44 @@ export default function addScrollEffect() {
 function scroll(event) {
   event.preventDefault();
   if (event.wheelDelta > 0) {
-    window.scroll({
-      top: window.pageYOffset - window.innerHeight,
-      behavior: 'smooth'
-    })
+    if (window.pageYOffset > window.innerHeight * 2.5 && window.pageYOffset < window.innerHeight * 3.5) {
+      window.scroll({
+        top: window.innerHeight * 2,
+        behavior: 'smooth'
+      })
+    }
+    if (window.pageYOffset > window.innerHeight * 1.5 && window.pageYOffset < window.innerHeight * 2.5) {
+      window.scroll({
+        top: window.innerHeight * 1,
+        behavior: 'smooth'
+      })
+    }
+    if (window.pageYOffset > window.innerHeight * 0.5 && window.pageYOffset < window.innerHeight * 1.5) {
+      window.scroll({
+        top: window.innerHeight * 0,
+        behavior: 'smooth'
+      })
+    }
   }
   if (event.wheelDelta < 0) {
-    window.scroll({
-      top: window.pageYOffset + window.innerHeight,
-      behavior: 'smooth'
-    })
+    console.log(1);
+    if (window.pageYOffset >= window.innerHeight * 0 && window.pageYOffset < window.innerHeight * 1) {
+      window.scroll({
+        top: window.innerHeight * 1,
+        behavior: 'smooth'
+      })
+    }
+    if (window.pageYOffset >= window.innerHeight * 1 && window.pageYOffset < window.innerHeight * 2) {
+      window.scroll({
+        top: window.innerHeight * 2,
+        behavior: 'smooth'
+      })
+    }
+    if (window.pageYOffset >= window.innerHeight * 2 && window.pageYOffset < window.innerHeight * 3) {
+      window.scroll({
+        top: window.innerHeight * 3,
+        behavior: 'smooth'
+      })
+    }
   }
 }
